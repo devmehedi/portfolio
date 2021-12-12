e84a950bbeb615ecc5661267d1728eae8bc10f61
devmehedi <mahedihasanshowrov1@gmail.com>
2021-12-12 23:03:48 +0600

    responsive

--- src/Pages/HomePage/Introduce/Introduce.js
+++ src/Pages/HomePage/Introduce/Introduce.js
@@ -2,57 +2,49 @@ import React from 'react';
 import mypic from '../../../images/Mypic.jpg';
 import { Typewriter } from 'react-simple-typewriter';
 import './Introduce.css'
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Introduce = () => {
     return (
-        <div class="">
-            <div class="intro">
-                <div class="container">
-                    <div className="d-flex justify-content-between text-white">
-                        <div class="col-6 text-start my-5">
-                            <h2 className='fw-bold'>MD. MEHEDI HASAN</h2>
-                            <h5>I'm A <span style={{ color: 'yellow', fontWeight: 'bold' }}>
-                                {/* Style will be inherited from the parent element */}
-                                <Typewriter
-                                    words={['Front end web developer❣️', 'Full stack web developer🖥️', 'MERN stack web developerⓂ️', 'React developer⚛️!']}
-                                    loop={10}
-                                    cursor
-                                    cursorStyle='(❁´◡`❁)'
-                                    typeSpeed={70}
-                                    deleteSpeed={50}
-                                    delaySpeed={1000}
-                                />
-                            </span></h5>
-                            <br />
-                            <address>
-                                Chattogram, Bangladesh.
-                            </address>
-                            <p>Email: mahedihasanshowrov1@gmail.com</p>
-                            <p>Phone: +8801878367418</p>
-                            <a href="/" class="mx-3 fs-1 social-icon"><i class="fab fa-github"></i></a>
-                            <a href="/" class="mx-3 fs-1 social-icon"><i class="fab fa-linkedin"></i></a>
-                            <a href="/" class="mx-3 fs-1 social-icon"><i class="fab fa-twitter-square"></i></a>
-                            <div>
-                                <a href="https://drive.google.com/file/d/1mKnjVxgJo15onX1ngC0IfkycBfVsSNTz/view?usp=sharing"><button className='btn my-5'>Resume</button></a>
-                            </div>
+        <div>
+            <Container className='intro'>
+                <Row>
+                    <Col md={6}>
+                        <h2 className='fw-bold'>MD. MEHEDI HASAN</h2>
+                        <h5>I'm A <span style={{ color: 'yellow', fontWeight: 'bold' }}>
+                            {/* Style will be inherited from the parent element */}
+                            <Typewriter
+                                words={['Front end web developer❣️', 'Full stack web developer🖥️', 'MERN stack web developerⓂ️', 'React developer⚛️!']}
+                                loop={10}
+                                cursor
+                                cursorStyle='(❁´◡`❁)'
+                                typeSpeed={70}
+                                deleteSpeed={50}
+                                delaySpeed={1000}
+                            />
+                        </span></h5>
+                        <br />
+                        <address>
+                            Chattogram, Bangladesh.
+                        </address>
+                        <p>Email: mahedihasanshowrov1@gmail.com</p>
+                        <p>Phone: +8801878367418</p>
+                        <a href="/" class="mx-3 fs-1 social-icon"><i class="fab fa-github"></i></a>
+                        <a href="/" class="mx-3 fs-1 social-icon"><i class="fab fa-linkedin"></i></a>
+                        <a href="/" class="mx-3 fs-1 social-icon"><i class="fab fa-twitter-square"></i></a>
+                        <div>
+                            <a href="https://drive.google.com/file/d/1mKnjVxgJo15onX1ngC0IfkycBfVsSNTz/view?usp=sharing"><button className='btn my-5'>Resume</button></a>
                         </div>
-                        <div class="col-6 my-5">
-                            <img style={{ width: '350px', borderRadius: '10%' }} src={mypic} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='container text-start my-5'>
-                <h3>Hello !</h3>
-                <p>I'm Mehedi, a Junior Front end Web developer. Solution-driven web developers specialize in contributing to a highly collaborative work environment,
-                    finding solutions, and determining customer satisfaction. Proven experience building user-centric websites
-                    using <span className='fw-bold'>HTML5, CSS3, Bootstrap, JS, React, React Router, Node JS, MongoDB, React Router Dom,
-                        React Hooks Form, MUI, Firebase Authentication, Error solving, and Debugging.</span> Creating web
-                    applications for desktop and mobile users. My goal is to meet the highest standards for web design and
-                    web applications built across multiple APIs, third-party integrations, and databases.</p>
-            </div>
+                    </Col>
+                    <Col md={6} className='my-5'>
+                        <img style={{ width: '350px', borderRadius: '10%' }} src={mypic} alt="" />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
 
 export default Introduce;
+
+
--- src/Pages/HomePage/Project/Project.js
+++ src/Pages/HomePage/Project/Project.js
@@ -1,26 +1,33 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 const Project = ({ project }) => {
     const { title, points1, points2, points3, images, technology, liveLink, clientGit, serverGit, linkedIn } = project;
     return (
-        <div className="d-flex justify-content-between my-4">
-            <div class="col-5 m-3 p-3">
-                <img style={{ width: '100%', margin: 10, alignItems: 'center' }} src={images} alt="" />
-            </div>
-            <div class="col-7 m-3 p-3 text-start">
-                <h3 className='mb-5'>⚛️ {title} _ _</h3>
-                <li className='list-unstyled'>{points1}</li>
-                <li className='list-unstyled'>{points2}</li>
-                <li className='list-unstyled'>{points3}</li><br />
-                <h6>{technology}</h6>
-                <a href={liveLink}><i class="fas fa-link fs-2 social-icon" data-bs-toggle="tooltip" data-bs-placement="top" title="Live Site"></i></a>
-                <a href={clientGit}><i class="fab fa-github-square fs-2 mx-5 social-icon" data-bs-toggle="tooltip" data-bs-placement="top" title="Live Site"></i></a>
-                <a href={serverGit}><i class="fab fa-node fs-2 social-icon" data-bs-toggle="tooltip" data-bs-placement="top" title="Live Site"></i></a>
-                <a href={linkedIn}><i class="fab fa-linkedin fs-2 mx-5 social-icon" data-bs-toggle="tooltip" data-bs-placement="top" title="Linked In"></i></a>
-            </div>
+        <div>
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <img style={{ width: '100%', margin: 10, alignItems: 'center' }} src={images} alt="" />
+                    </Col>
+                    <Col md={6} className='my-5'>
+                        <h3 className='mb-5'>⚛️ {title} _ _</h3>
+                        <li className='list-unstyled'>{points1}</li>
+                        <li className='list-unstyled'>{points2}</li>
+                        <li className='list-unstyled'>{points3}</li><br />
+                        <h6>{technology}</h6>
+                        <a href={liveLink}><i class="fas fa-link fs-2 social-icon" data-bs-toggle="tooltip" data-bs-placement="top" title="Live Site"></i></a>
+                        <a href={clientGit}><i class="fab fa-github-square fs-2 mx-5 social-icon" data-bs-toggle="tooltip" data-bs-placement="top" title="Live Site"></i></a>
+                        <a href={serverGit}><i class="fab fa-node fs-2 social-icon" data-bs-toggle="tooltip" data-bs-placement="top" title="Live Site"></i></a>
+                        <a href={linkedIn}><i class="fab fa-linkedin fs-2 mx-5 social-icon" data-bs-toggle="tooltip" data-bs-placement="top" title="Linked In"></i></a>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
 
 export default Project;
+
+
 
 
